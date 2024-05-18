9aaa9b06e93a04ccb32145f975f0a651119e3c3c
sayeshalmashi <152030425+sayeshalmashi@users.noreply.github.com>
2024-05-18 17:04:40 +0330

    admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from blog.models import Movie
 # Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+  date_hierarchy='create_date'
+  empty_value_display='-empty-'
+  list_display=('title','status','published_date','create_date',)
+  list_filter=('status',) #topple akharesh ,
+  ordering=['-create_date']
+  search_fields=['title','content']
+
+admin.site.register(Movie,MovieAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
 
 class Movie(models.Model):
   title=models.CharField(max_length=255)
-  
+  content=models.TextField()
+  rate=models.IntegerField()
+  status=models.BooleanField(default=False)
+  published_date=models.DateTimeField(null=True)
+  create_date=models.DateTimeField(auto_now_add=True)
+  update_date=models.DateTimeField(auto_now=True)
+  class Meta:
+    ordering=['-create_date']
+    
+  def __str__(self):
+   return "{}-{}".format(self.title,self.id)
